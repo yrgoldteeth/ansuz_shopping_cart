16551f85dfdce0bc7bcdae30cdad9a4303f1e2bb
Nicholas Fine <nicholas.fine@gmail.com>
2009-06-19 10:03:00 -0500

    removed extraneous files from old product manager plugin

--- routes.rb
+++ routes.rb
@@ -1,7 +1,2 @@
-namespace :admin do |admin|
-  admin.resources :products, :has_many => [:quantity_discounts]
-  admin.resources :categories
-#  admin.resources :quantity_discounts, :belongs_to => [:products]
-end
 resources :products
 resource :cart, :member => [:add]
